cbdaa409dbf72cd6b9c3b59353fedf1766ceda49
Mehboob-Coder <ahma41980@gmail.com>
2024-08-21 09:56:44 +0500

    mehboob

--- q1/settings.py
+++ q1/settings.py
@@ -122,10 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT =587
-EMAIL_USE_TLS= True
-EMAIL_HOST_USER= 'djangotesting5@gmail.com'
-EMAIL_HOST_PASSWORD = 'vkehfsazzoquntlw'
+from q11.env import *
+EMAIL_BACKEND = EMAIL_BACKEND 
+EMAIL_HOST = EMAIL_HOST
+EMAIL_PORT =EMAIL_PORT
+EMAIL_USE_TLS= EMAIL_USE_TLS
+EMAIL_HOST_USER= EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
